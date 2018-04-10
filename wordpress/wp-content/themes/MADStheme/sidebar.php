23f2e6b908459331220fff96b9911d48eebb4868
haulindaphne <haulindaphne@gmail.com>
2018-04-10 11:36:03 +0200

    archives responsive update

--- wordpress/wp-content/themes/MADStheme/sidebar.php
+++ wordpress/wp-content/themes/MADStheme/sidebar.php
@@ -61,17 +61,17 @@
                 <ul>
                     <li class="fcbk">
                         <a href="https://fr-fr.facebook.com/">
-                            <img class="social" src="http://localhost/MADS-foodog/wordpress/wp-admin/images/facebook.png" alt="facebook">
+                            <img class="social" src="/MADS-foodog/wordpress/wp-content/themes/MADStheme/images/facebook-blanc.png" alt="facebook">
                         </a>21 562 Fans <span>LIKE</span>
                     </li>
                     <li class="twitter">
                         <a href="https://twitter.com/?lang=fr">
-                            <img class="social" src="http://localhost/MADS-foodog/wordpress/wp-admin/images/twitter.png" alt ="twitter">
+                            <img class="social" src="/MADS-foodog/wordpress/wp-content/themes/MADStheme/images/twitter-blanc.png" alt ="twitter">
                         </a>1 401 Followers <span>FOLLOW</span>
                     </li>
                     <li class="insta">  
                         <a href="https://www.instagram.com/?hl=fr">
-                            <img class="social" src="http://localhost/MADS-foodog/wordpress/wp-admin/images/insta.png" alt="instagram">
+                            <img class="social" src="/MADS-foodog/wordpress/wp-content/themes/MADStheme/images/insta-blanc.png" alt="instagram">
                         </a>23 538 Followers <span>FOLLOW</span>
                     </li>
                 </ul>
